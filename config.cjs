4a616fa30cdfda3c90c92b8fc8cceaaa5c8b4a8d
ben509-bo <c12386791@gmail.com>
2025-06-28 12:02:03 -0400

    Update config.cjs
    Signed-off-by: ben509-bo <c12386791@gmail.com>

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "INCONNU~XD~f1JXVRLY#62wWwf21g83beAUusTZGGLcTI7iokrE_2TnSlkjLIaE",
   PREFIX: process.env.PREFIX || '.',
    BOT_NAME: process.env.BOT_NAME || "INCONNU XD V2",
    BOT: process.env.BOT || "hello 👋",
